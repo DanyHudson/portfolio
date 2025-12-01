d7a1be63c88a8aca66a22286164c8e0ec42db7f7
DanyHudson <mail.d.hudson@gmail.com>
2025-12-01 16:18:23 +0100

    added/changed styles

--- src/app/page/page.ts
+++ src/app/page/page.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 // import { CommonModule } from '@angular/common';
 import { LandingPage } from '../landing-page/landing-page';
-import { Nav } from '../nav/nav';
+// import { Nav } from '../nav/nav';
 import { PageContent } from "../page-content/page-content";
 
 
 
 @Component({
   selector: 'app-page',
-  imports: [LandingPage, Nav, PageContent],
+  imports: [LandingPage, PageContent],
   standalone: true,
   templateUrl: './page.html',
   styleUrls: ['./page.scss'],
