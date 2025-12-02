fc7e8bf285c89f72e88b179f1e9ebb7418259430
DanyHudson <mail.d.hudson@gmail.com>
2025-12-02 16:07:32 +0100

    adding styles and elements

--- src/app/page-content/page-content.ts
+++ src/app/page-content/page-content.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AboutMe } from '../about-me/about-me';
+import { MySkills } from '../my-skills/my-skills';
 
 @Component({
   selector: 'app-page-content',
   standalone: true,
-  imports: [AboutMe],
+  imports: [AboutMe, MySkills],
   templateUrl: './page-content.html',
   styleUrls: ['./page-content.scss'],
 })
