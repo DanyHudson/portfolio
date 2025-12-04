828ea518c4f1fbb7a6090a2d2a13a557b7042d23
DanyHudson <mail.d.hudson@gmail.com>
2025-12-04 15:07:17 +0100

    added my-projects component

--- src/app/page-content/page-content.ts
+++ src/app/page-content/page-content.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { AboutMe } from '../about-me/about-me';
 import { MySkills } from '../my-skills/my-skills';
+import { MyProjects } from '../my-projects/my-projects';
+
 
 @Component({
   selector: 'app-page-content',
   standalone: true,
-  imports: [AboutMe, MySkills],
+  imports: [AboutMe, MySkills, MyProjects],
   templateUrl: './page-content.html',
   styleUrls: ['./page-content.scss'],
 })
