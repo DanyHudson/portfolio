bdfc6bb67e8b4e39f9be913c427b61e2f73eec8c
DanyHudson <mail.d.hudson@gmail.com>
2025-12-05 12:26:16 +0100

    still trying to style mat design tabs menu

--- src/app/tabs/tabs.ts
+++ src/app/tabs/tabs.ts
@@ -12,14 +12,16 @@ import { MatTabsModule } from '@angular/material/tabs';
 export class Tabs {
   // tabs = ['First', 'Second', 'Third'];
   tabs = [
-  {
-     label: 'Tab 1',
-     content: 'Content for Tab 1 lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua,Content for Tab 1 lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' },
-  { label: 'Tab 2', content: 'Content for Tab 2' },
-  { label: 'Tab 3', content: 'Content for Tab 3' }
-];
+    {
+      label: 'Tab 1',
+      content: 'Content for Tab 1 lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua,Content for Tab 1 lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    },
+
+    { label: 'Tab 2', content: 'Content for Tab 2' },
+    { label: 'Tab 3', content: 'Content for Tab 3' }
+  ];
   activeLink = this.tabs[0];
-  
+
 
   // addLink() {
   //   this.tabs.push(`Link ${this.tabs.length + 1}`);
