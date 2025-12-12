e86ed2ba77956a43fc8a63eec93194b161ae9988
DanyHudson <mail.d.hudson@gmail.com>
2025-12-12 21:22:30 +0100

    renamed component

---
+++ src/app/projects/project.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project',
+  standalone: true,
+  imports: [],
+  templateUrl: './project.html',
+  styleUrls: ['./project.scss'],
+})
+export class Project {
+
+
+
+  
+
+}
