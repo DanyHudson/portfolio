bc710283638afe05afe5caf9b5a8f79f001bfd31
DanyHudson <mail.d.hudson@gmail.com>
2025-12-28 21:54:43 +0100

    added testimonial component

--- src/app/page-content/page-content.ts
+++ src/app/page-content/page-content.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { AboutMe } from '../about-me/about-me';
 import { MySkills } from '../my-skills/my-skills';
 import { MyProjects } from '../my-projects/my-projects';
+import { Testimonials } from '../testimonials/testimonials';
 
 
 @Component({
   selector: 'app-page-content',
   standalone: true,
-  imports: [AboutMe, MySkills, MyProjects],
+  imports: [AboutMe, MySkills, MyProjects, Testimonials],
   templateUrl: './page-content.html',
   styleUrls: ['./page-content.scss'],
 })
