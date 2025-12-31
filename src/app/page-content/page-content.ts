0233f991cdc090b136e2a55865a35bce014cee46
DanyHudson <mail.d.hudson@gmail.com>
2025-12-31 20:14:56 +0100

    imported contact

--- src/app/page-content/page-content.ts
+++ src/app/page-content/page-content.ts
@@ -3,12 +3,14 @@ import { AboutMe } from '../about-me/about-me';
 import { MySkills } from '../my-skills/my-skills';
 import { MyProjects } from '../my-projects/my-projects';
 import { Testimonials } from '../testimonials/testimonials';
+// import { ContactForm } from '../contact-form/contact-form';
+import { Contact } from '../contact/contact';
 
 
 @Component({
   selector: 'app-page-content',
   standalone: true,
-  imports: [AboutMe, MySkills, MyProjects, Testimonials],
+  imports: [AboutMe, MySkills, MyProjects, Testimonials, Contact],
   templateUrl: './page-content.html',
   styleUrls: ['./page-content.scss'],
 })
