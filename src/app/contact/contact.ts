57d4cf865e58754dd0d7ae366c009811c95ca15d
DanyHudson <mail.d.hudson@gmail.com>
2025-12-31 20:14:09 +0100

    first commit

---
+++ src/app/contact/contact.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ContactForm } from '../contact-form/contact-form';
+
+@Component({
+  selector: 'app-contact',
+  imports: [ContactForm],
+  templateUrl: './contact.html',
+  styleUrl: './contact.scss',
+})
+export class Contact {
+
+}
