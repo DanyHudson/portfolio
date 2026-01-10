2fd40d368ba7ce8b1a8faf89b823f4c5d5448bef
DanyHudson <mail.d.hudson@gmail.com>
2026-01-10 21:19:18 +0100

    fixed background image for selected links

--- src/app/nav/nav.ts
+++ src/app/nav/nav.ts
@@ -2,10 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
+  standalone: true,
   imports: [],
   templateUrl: './nav.html',
   styleUrls: ['./nav.scss'],
 })
 export class Nav {
+  constructor() { }
+  navLinks = [
+    { label: 'About me', anchor: 'aboutme' },
+    { label: 'Skills', anchor: 'skills' },
+    { label: 'Projects', anchor: 'projects' },
+    { label: 'Contact', anchor: 'contact' }
+  ];
 
-}
+  selectedLink: string = '';
+
+  onNavClick(link: { label: string; anchor: string }) {
+    this.selectedLink = link.label;
+  }
+} 
