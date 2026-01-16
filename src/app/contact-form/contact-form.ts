9f2c22f316fcd60ae89602cfee8f0670b53261b1
DanyHudson <mail.d.hudson@gmail.com>
2026-01-16 13:25:44 +0100

    working on contact form logic

--- src/app/contact-form/contact-form.ts
+++ src/app/contact-form/contact-form.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.scss',
 })
 export class ContactForm {
 
+  onSubmit() {
+    alert('Thank you for your message. I will get back to you soon!');
+  } 
 }
