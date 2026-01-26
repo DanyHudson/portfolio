120d542112890317a351268f4f4f97b9340a4f53
DanyHudson <mail.d.hudson@gmail.com>
2026-01-26 13:40:15 +0100

    added http client and email logic

--- src/app/contact-form/contact-form.ts
+++ src/app/contact-form/contact-form.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-form',
@@ -10,6 +11,8 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class ContactForm {
 
+  http = inject(HttpClient);
+
   contactFormData = {
     userName: '',
     userEmail: '',
@@ -17,13 +20,46 @@ export class ContactForm {
     privacyPolicyAccepted: false,
   };
 
-  onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted && ngForm.touched) {
-      console.log('Form Data: ', ngForm.value);
-    }
-    else {
-      console.log(' fill out the form correctly ');
-    }
+  // onSubmit(ngForm: NgForm) {
+  //   if (ngForm.valid && ngForm.submitted && ngForm.touched) {
+  //     console.log('Form Data: ', ngForm.value);
+  //   }
+  //   else {
+  //     console.log(' fill out the form correctly ');
+  //   }
 
+  // }
+
+  mailTest = true; // set to false for real mail sending
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) { // remove ' !this.mailTest' for real mail sending
+      this.http.post(this.post.endPoint, this.post.body(this.contactFormData))
+        .subscribe({
+          next: (response) => {
+            // insert here what else should happen on success, e.g. a success message
+
+            ngForm.resetForm();
+          },
+          error: (error: any) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) { // delete this for real mail sending
+      // asa above insert here what else should happen on success, e.g. a success message
+      ngForm.resetForm();
+    }
   }
 }
