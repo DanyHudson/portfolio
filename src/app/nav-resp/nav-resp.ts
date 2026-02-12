ea10b13d2d1054f3c91d69102cd4b167caf6d742
DanyHudson <mail.d.hudson@gmail.com>
2026-02-12 15:37:48 +0100

    first commit, components for responsive design

---
+++ src/app/nav-resp/nav-resp.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-resp',
+  imports: [],
+  templateUrl: './nav-resp.html',
+  styleUrl: './nav-resp.scss',
+})
+export class NavResp {
+
+}
