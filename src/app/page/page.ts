c070b6ba2c74c2ffc7212b236567db9ce673b4fe
DanyHudson <mail.d.hudson@gmail.com>
2026-04-07 16:25:39 +0200

    logic and styles for  priv-pol component

--- src/app/page/page.ts
+++ src/app/page/page.ts
@@ -1,24 +1,18 @@
 import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
 import { LandingPage } from '../landing-page/landing-page';
 import { Nav } from '../nav/nav';
-import { NavResp } from '../nav-resp/nav-resp';
 import { PageContent } from "../page-content/page-content";
 import { Footer } from '../footer/footer';
 import { LandingPageResp } from "../landing-page-resp/landing-page-resp";
 
-
-
-
 @Component({
   selector: 'app-page',
-  imports: [LandingPage, Nav, NavResp, PageContent, Footer, LandingPageResp],
   standalone: true,
+  imports: [LandingPage, Nav, PageContent, Footer, LandingPageResp],
   templateUrl: './page.html',
   styleUrls: ['./page.scss'],
   
 })
 
 export class Page {
-
 }
