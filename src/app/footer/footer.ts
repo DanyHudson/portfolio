805f1fc4e0ab6c92d1540fbbc3f3a60549377501
DanyHudson <mail.d.hudson@gmail.com>
2026-04-08 16:06:13 +0200

    adding legal notice component

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { SocMed } from '../soc-med/soc-med';
-import { LegalNotice } from '../legal/legal-notice/legal-notice';
+// import { LegalNotice } from '../legal/legal-notice/legal-notice';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [SocMed, LegalNotice],
+  imports: [SocMed],
   templateUrl: './footer.html',
   styleUrls: ['./footer.scss'],
 })
 export class Footer {
+
+  // legelNotice = LegalNotice
+  @Output() legalNoticeRequested = new EventEmitter<void>();  
+
+   requestLegalNotice() {
+    this.legalNoticeRequested.emit();
+  }
 
 }
