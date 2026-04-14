7c37863bb342891220d8bb4f330fcd9eeed4cb9b
DanyHudson <mail.d.hudson@gmail.com>
2026-04-14 22:34:18 +0200

    new styles for nav-resp

--- src/app/nav-resp/nav-resp.ts
+++ src/app/nav-resp/nav-resp.ts
@@ -1,50 +1,3 @@
-// import { Component, EventEmitter, Output } from '@angular/core';
-// import { LangService } from '../services/lang.service';
-
-// @Component({
-//   selector: 'app-nav-resp',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './nav-resp.html',
-//   styleUrls: ['./nav-resp.scss'],
-// })
-
-// export class NavResp {
-//   @Output() menuOpenChange = new EventEmitter<boolean>();
-
-//   navLinks = [
-//     { label: 'About me', anchor: 'aboutme' },
-//     { label: 'Skills', anchor: 'skills' },
-//     { label: 'Projects', anchor: 'projects' },
-//     { label: 'Contact', anchor: 'contact' }
-//   ];
-
-//   selectedLink: string = '';
-//   currentLang: string = 'en';
-
-//   navOpen = false;
-
-//   toggleNav(nextState?: boolean) {
-//     this.navOpen = nextState ?? !this.navOpen;
-//     this.menuOpenChange.emit(this.navOpen);
-//   }
-
-//   constructor(private langService: LangService) {
-//     this.langService.lang$.subscribe(lang => this.currentLang = lang);
-//   }
-
-//   setLanguage(lang: string) {
-//     this.langService.setLang(lang as 'en' | 'de');
-//   }
-
-//   onNavClick(link: { label: string; anchor: string }) {
-//     this.selectedLink = link.label;
-//     this.toggleNav(false);
-//   }
-
-// }
-
-// ::: model() version :::
 import { Component, model } from '@angular/core';
 import { LangService } from '../services/lang.service';
 
@@ -65,7 +18,6 @@ export class NavResp {
     { label: 'Contact', anchor: 'contact' }
   ];
 
-  selectedLink: string = '';
   currentLang: string = 'en';
 
   constructor(private langService: LangService) {
@@ -84,8 +36,12 @@ export class NavResp {
     this.langService.setLang(lang as 'en' | 'de');
   }
 
-  onNavClick(link: { label: string; anchor: string }) {
-    this.selectedLink = link.label;
+  // onNavClick(link: { label: string; anchor: string }) {
+  //   this.selectedLink = link.label;
+  //   this.closeNav();
+  // }
+
+  onNavClick(): void {
     this.closeNav();
   }
 }
