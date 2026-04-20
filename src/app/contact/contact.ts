f58cbd874ad650ecd400bad92527ef3545fcf334
DanyHudson <mail.d.hudson@gmail.com>
2026-04-20 16:21:12 +0200

    fixing error signaling behaviour

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -13,13 +13,13 @@ export class Contact {
   contact = contactInfo[0];
   @Output() privacyPolicyRequested = new EventEmitter<void>();
 
-/**
- * Smoothly scrolls to the given target element and then back to the exact position after a short delay.
- * This is useful for creating a "bounce" effect when scrolling to an element.
- *
- * @param targetId The ID of the target element to scroll to.
- * @param event The event that triggered the scroll, passed for convenience.
- */
+  /**
+   * Smoothly scrolls to the given target element and then back to the exact position after a short delay.
+   * This is useful for creating a "bounce" effect when scrolling to an element.
+   *
+   * @param targetId The ID of the target element to scroll to.
+   * @param event The event that triggered the scroll, passed for convenience.
+   */
   onBounceScroll(targetId: string, event: Event): void {
     event.preventDefault();
     const target = document.getElementById(targetId);
@@ -35,10 +35,16 @@ export class Contact {
     }, 400);
   }
 
-/**
- * Emits the privacyPolicyRequested event when the user requests to view the privacy policy.
- */
+  /**
+   * Emits the privacyPolicyRequested event when the user requests to view the privacy policy.
+   */
   onPrivacyPolicyRequested() {
     this.privacyPolicyRequested.emit();
   }
+
+  onScrollToTop(event: Event): void {
+    const targetId = window.innerWidth <= 1020 ? 'persona-resp' : 'persona';
+    this.onBounceScroll(targetId, event);
+  }
+
 }
